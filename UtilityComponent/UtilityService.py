6ca8970e1bc0c15b9d7f60fcbfd85c4e2de41e3f
StefanostheDev <stefanosdev26@gmail.com>
2023-08-28 19:42:03 -0700

    Pushing latest

--- UtilityComponent/UtilityService.py
+++ UtilityComponent/UtilityService.py
@@ -3,12 +3,17 @@ class UtilityService:
         filteredVariable = "".join(value.split())
         return filteredVariable
     
-    def can_be_integer(x):
-        try:
-            int(x)
-            return True
-        except ValueError:
-            return False
-            
-    
-    
+    def validate_input_for_Id(request_id):
+        while True:
+            if not request_id:
+                print('Request Id is null')
+                continue  # Go back to the start of the loop
+            if request_id.lower() == 'exit':
+                    break  # Exit the loop
+            parsedID = UtilityService.removeAllSpaces(request_id)  # Check for spaces. ID coming in
+            try:
+                parsedID = int(parsedID)  # This will raise a ValueError if conversion fails
+            except ValueError: 
+                print(f"'{parsedID}' is not a valid integer ID.")
+                continue  # Go back to the start of the loop
+        return
