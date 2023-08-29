e89ede8047f8de6cd1f06cbe08222a12126a96b2
StefanosTheDev <stefanosdev26@gmail.com>
2023-08-28 21:29:10 -0700

    pushing some more changes related to user and working on create user

--- AdminComponent/AdminService.py
+++ AdminComponent/AdminService.py
@@ -3,12 +3,61 @@ from Database.db_setup import Session  # Assuming Session is exposed from db_set
 from sqlalchemy.orm.exc import NoResultFound
 from UtilityComponent.UtilityService import UtilityService
 from sqlalchemy.exc import SQLAlchemyError
-
+from validate_email import validate_email
 class AdminService:
+    #**TODO Lets handle some casing. 
+    #**TODO for this feature:
     
+    
+    #**TODO  (Username Features)
+    def create_user_validation(username, password, email, role):
+        if len(username) > 5 and len(username) < 12:
+            print(f"{username} fits the length requirement")
+        upper_lower_list = [letter for letter in username if letter.isupper() or letter.islower()]
+        if upper_lower_list:
+            print("this is valid")
+        else: 
+            print("this is not valid")
+    
+    #  5-12 charachters
+    # - 1 Upper Case One LowerCase Letter
+    # - Restrict Special Charachters
+    # - No Spaces
+    # - Uniquness. in the database
+    # Make sure words like admin / support / owner are not able to be usernames
+    
+    
+    
+    #**TODO (Password Features)
+    # 8 Charachters Special minimum . Max 15
+    # 1 Upper Case 1 Lower Case
+    # 1 Special Charachters
+    # No Spaces
+    
+    #**TODO (Email Features)
+    #
+    #
+    #
+    #
+    
+    
+    
+    
+    #**Password -> 8 charachters special.  (encrypt this )
+    #** Email -> Make sure its a valid email
+        #- Maybe it will take the Username, and generate a unique identifier for the Email. 
+        # 
+    #** Role Give an option to chose existing roles. if you just want to add to that hot key.
+    #   - Maybe in this have a dictionary Key so they can see whats going on. 
     def create_user():
         session = Session()
         username = input("Username:")
+        
+        
+        
+        
+        
+        
         password = input("Password:")
         email = input("Email:")
         role = input("Role:")
@@ -17,6 +66,10 @@ class AdminService:
         session.add(new_user)
         session.commit()
         session.close()
+        
+        
+        
+        
     def display_users():
         session = Session()
         try: 
@@ -27,8 +80,6 @@ class AdminService:
                   print(user.id, user.username, user.email, user.role)
         except Exception as error:
             print(error)
-
-        
         print(all_users)
     def handle_user_input():
         request_id = input("Enter ID to search for (or type 'exit' to quit): ")  # Gets ID
@@ -42,8 +93,7 @@ class AdminService:
             return int(parsedID)  # This will raise a ValueError if conversion fails
         except ValueError:
             print("Invalid ID format. Please enter a numeric ID.")
-            # Continues to the next iteration of the loop to prompt user again
-
+            # Continues to the next iteration of the loop to prompt user again        
     def return_user_by_Id():
         while True:
             session = Session()
@@ -64,6 +114,6 @@ class AdminService:
                 print(f"Database Error: {e}")
             finally:
                 session.close()  # close the session
-
+    
     
     
